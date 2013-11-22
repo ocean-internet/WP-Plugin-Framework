5710d137be586d951bd60b03eaaba4893963b953
Andy Weir <andy@uknetweb.com>
2013-11-22 17:06:02 +0000

    more re-shuffling

--- MyPlugin/my-plugin.php
+++ MyPlugin/my-plugin.php
@@ -1,23 +1,34 @@
 <?php
 /**
- * MyPlugin Name: MyPlugin
- * Description: Description of MyPlugin
+ * Name:        MyPlugin
+ * Description: DescriptionOfMyPlugin
  */
 
-$oisSrcDir       = plugin_dir_path(__FILE__ ) . 'src' . DS;
-$oisFrameworkDir = $oisSrcDir . 'WpPluginFramework' . DS;
-$oisPluginDir    = $oisSrcDir . 'MyPlugin' . DS;
+if(!defined('DS')) {
+
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+if(!defined('OIS_WPPF')) {
+
+    define('OIS_WPPF', TRUE);
+
+    define('OIS_WPPF_DIR',           dirname(__FILE__));
+    define('OIS_WPPF_SCRIPTS_DIR',   OIS_WPPF_DIR . DS . 'scripts');
+    define('OIS_WPPF_TEMPLATES_DIR', OIS_WPPF_DIR . DS . 'templates');
+    define('OIS_WPPF_VENDOR_DIR',    OIS_WPPF_DIR . DS . 'vendor');
+
+    define('OIS_WPPF_FRAMEWORK_DIR', OIS_WPPF_VENDOR_DIR . DS . 'src' . DS . 'ocean-internet' . DS . 'wp-plugin-framework');
+}
 
 // Include the Composer autoloader
-require_once($oisSrcDir . 'Vendor' . DS . 'autoload.php');
+require_once(OIS_WPPF_VENDOR_DIR . DS . 'autoload.php');
 
-// Include the Framework bootstrap
-require_once($oisFrameworkDir . 'Config' . DS . 'bootstrap.php');
-
-// Include the Framework functions
-require_once($oisPluginFrameworkDir . 'Config' . DS . 'functions.php');
+// Include the Framework config
+require_once(OIS_WPPF_FRAMEWORK_DIR . DS . 'Config'. DS . 'bootstrap.php');
+require_once(OIS_WPPF_FRAMEWORK_DIR . DS . 'Config'. DS . 'functions.php');
 
 // Include the MyPlugin
-require_once($oisPluginDir . 'Config' . DS . 'pluggable.php');
-require_once($oisPluginDir . 'Config' . DS . 'actions.php');
-require_once($oisPluginDir . 'Config' . DS . 'filters.php');
+require_once(OIS_WPPF_SCRIPTS_DIR . DS . 'pluggable.php');
+require_once(OIS_WPPF_SCRIPTS_DIR . DS . 'actions.php');
+require_once(OIS_WPPF_SCRIPTS_DIR . DS . 'filters.php');
